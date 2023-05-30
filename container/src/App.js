edb71521751e11ad22caeed1ee5fc20ef500618a
Ganesh Nikam <ganeshni@synerzipune.local>
2023-05-30 17:31:45 +0530

    Updated invalidation variable

--- container/src/App.js
+++ container/src/App.js
@@ -5,7 +5,7 @@ import RemoteVueApp from "./components/RemoteVueApp";
 export const App = () => {
   return (
     <div style={{ border: '1px solid black' , padding: '10px'}}>
-      <h1>This is React app with Webpack. It is continer App.</h1>
+      <h1>This is React app. It is continer App.</h1>
         <div  style={{ border: '1px solid blue' , padding: '10px', margin: '10px'}}>
               <h2>MFE -One</h2>
              <RemoteReactApp />
