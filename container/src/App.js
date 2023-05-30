69f7af1fe6795ec5d98b9d78a099f501fd6a7602
Ganesh Nikam <ganeshni@synerzipune.local>
2023-05-30 17:29:00 +0530

    Updated Text

--- container/src/App.js
+++ container/src/App.js
@@ -7,11 +7,11 @@ export const App = () => {
     <div style={{ border: '1px solid black' , padding: '10px'}}>
       <h1>This is React app with Webpack. It is continer App.</h1>
         <div  style={{ border: '1px solid blue' , padding: '10px', margin: '10px'}}>
-              <h2>MFE -1</h2>
+              <h2>MFE -One</h2>
              <RemoteReactApp />
         </div>
         <div  style={{ border: '1px solid red' , padding: '10px', margin: '10px'}}>
-              <h2>MFE -2</h2>
+              <h2>MFE -two</h2>
              <RemoteVueApp />
         </div>
     </div>
